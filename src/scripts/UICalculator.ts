57ba641e74a58cc824d9f706d56491fe00a0d016
Igor <igor06brw@gmail.com>
2020-09-05 17:17:37 +0200

    Generate and display one button

--- src/scripts/UICalculator.ts
+++ src/scripts/UICalculator.ts
@@ -1,6 +1,21 @@
 class UICalculator {
-    button: string;
-    value: number;
-    action: string;
-    operator: string;
+    app: HTMLElement;
+    button: HTMLButtonElement;
+
+
+    constructor(app: HTMLElement) {
+        this.app = app;
+    }
+    generateDigitButton() {
+            this.button = document.createElement('button');
+            this.button.type = 'button';
+            this.button.textContent = 'Button';
+            this.app.appendChild(this.button);
+    }
+}
+
+window.onload = () => {
+    const app: HTMLElement = document.getElementById('app');
+    const calc = new UICalculator(app);
+    calc.generateDigitButton();
 }
