42a2012952fc0ebd43c9228c29ed13ce0b9ea4c2
Igor <igor06brw@gmail.com>
2020-09-06 04:40:07 +0200

    Add generate HTML class and actions, decimal buttons

--- src/scripts/UICalculator.ts
+++ src/scripts/UICalculator.ts
@@ -6,22 +6,28 @@ class UICalculator {
     constructor(app: HTMLElement) {
         this.app = app;
     }
-    generateButton(value: string, id: string, name: string) {
+    generateButton(value: string, id: string, name: string, HTMLClass: string) {
         this.button = document.createElement('button');
         this.button.type = 'button';
         this.button.textContent = value;
-        this.button.id = id
+        this.button.id = id;
         this.button.name = name; 
+        this.button.className = HTMLClass;
         this.app.appendChild(this.button);
     }
     displayButtons() {
         for(let i = 0; i <= 9; i++) {
-            this.generateButton(String(i), 'button' + i, String(i));
+            this.generateButton(String(i), 'button' + i, String(i), 'button digit');
         }
-        this.generateButton('-', 'substraction', 'minus');
-        this.generateButton('+', 'addition', 'plus');
-        this.generateButton('*', 'multiply', 'multiple');
-        this.generateButton('/', 'divide' , 'divide');
+        this.generateButton('-', 'substraction', 'minus', 'button operator');
+        this.generateButton('+', 'addition', 'plus', 'button operator');
+        this.generateButton('*', 'multiply', 'multiple', 'button operator');
+        this.generateButton('/', 'divide' , 'divide', 'button operator');
+
+        this.generateButton('=', 'equal', 'result', 'button action');
+        this.generateButton('AC', 'clean', 'reset', 'button action');
+        
+        this.generateButton('.', 'decimal', 'decimal', 'button decimal');
     }
 }
 
@@ -29,7 +35,4 @@ window.onload = () => {
     const app: HTMLElement = document.getElementById('app');
     const calc = new UICalculator(app);
     calc.displayButtons();
-
-    const result =  1 + 1 * 2;
-    console.log(result);
 }
