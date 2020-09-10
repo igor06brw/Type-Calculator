2d88aac26fbdf158db42c1dca1772bcd8e45b066
Igor <igor06brw@gmail.com>
2020-09-10 18:15:18 +0200

    Center calculator

--- src/scripts/UICalculator.ts
+++ src/scripts/UICalculator.ts
@@ -33,30 +33,30 @@ export class UICalculator {
     }
 
     displayButtons() {
-        this.generateButton('AC', 'clean', 'clear', 'button action');
-        this.generateButton(' ', ' ', ' ', 'button operator');
-        this.generateButton(' ', ' ', ' ', 'button operator');
-        this.generateButton('/', 'divide' , 'divide', 'button operator');
+        this.generateButton('AC', 'clean', 'clear', 'button button--action button--crear');
+        this.generateButton(' ', ' ', ' ', 'button blank');
+        this.generateButton(' ', ' ', ' ', 'button blank');
+        this.generateButton('/', 'divide' , 'divide', 'button butto--operator button--divide');
 
         this.generateButton('7', 'button7', '7', 'button button--7');
         this.generateButton('8', 'button8', '8', 'button button--8');
         this.generateButton('9', 'button9', '9', 'button button--9');
-        this.generateButton('*', 'multiply', 'multiple', 'button operator');
+        this.generateButton('*', 'multiply', 'multiple', 'button button--operator button--multiple');
 
         this.generateButton('4', 'button4', '4', 'button button--4');
         this.generateButton('5', 'button5', '5', 'button button--5');
         this.generateButton('6', 'button6', '6', 'button button--6');
-        this.generateButton('-', 'substraction', 'substrace', 'button operator');
+        this.generateButton('-', 'substraction', 'substrace', 'button button--operator button--substrace');
 
         this.generateButton('1', 'button1', '1', 'button button--1');
         this.generateButton('2', 'button2', '2', 'button button--2');
         this.generateButton('3', 'button3', '3', 'button button--3');
-        this.generateButton('+', 'addition', 'add', 'button operator');
+        this.generateButton('+', 'addition', 'add', 'button button--operator button--addition');
 
         this.generateButton('0', 'button0', '0', 'button button--0');
 
-        this.generateButton('.', 'decimal', 'decimal', 'button decimal');
-        this.generateButton('=', 'equal', 'equal', 'button action');
+        this.generateButton('.', 'decimal', 'decimal', 'button button--decimal');
+        this.generateButton('=', 'equal', 'equal', 'button button--action button--equal');
         
     }
     displayOutput() {
